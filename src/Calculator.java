df8a427cb12262f36d6ba57c8d5c712a183337bb
ruptis <90418759+ruptis@users.noreply.github.com>
2021-09-28 22:58:47 +0300

    28 09 2021 N3

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,16 +8,12 @@ public class Calculator implements ICalculator {
         expression = expression
                 .replace(" ", "")
                 .replaceFirst("^-", "0-")
-                .replace("(-", "(0-");
+                .replace("\\(-", "(0-");
 
-        String itemsRPN = getReversePolishNotation(getItems(expression)).toString();
-
-        System.out.println(itemsRPN);
-
-        return 0;
+        return getAnswer(getReversePolishNotation(getItems(expression)));
     }
 
-    private ArrayList<String> getItems(String expression) {
+    private ArrayList<String> getItems(String expression){
         ArrayList<String> items = new ArrayList<>();
 
         String regex = "([0-9]+[.]?[0-9]*)|[+\\-*^/()]";
@@ -26,7 +22,7 @@ public class Calculator implements ICalculator {
 
         Matcher matcher = pattern.matcher(expression);
 
-        while (matcher.find()) {
+        while (matcher.find()){
             items.add(matcher.group());
         }
 
@@ -86,6 +82,45 @@ public class Calculator implements ICalculator {
         }
 
         return itemsRPN;
+    }
+
+    private double getAnswer(ArrayList<String> itemsRPN){
+        double temp;
+
+        while (itemsRPN.size() > 1) {
+            for (int i = 0; i < itemsRPN.size(); i++) {
+                switch (itemsRPN.get(i)) {
+                    case "+": {
+                        temp = Double.parseDouble(itemsRPN.get(i - 2)) + Double.parseDouble(itemsRPN.get(i - 1));
+                        break;
+                    }
+                    case "-": {
+                        temp = Double.parseDouble(itemsRPN.get(i - 2)) - Double.parseDouble(itemsRPN.get(i - 1));
+                        break;
+                    }
+                    case "*": {
+                        temp = Double.parseDouble(itemsRPN.get(i - 2)) * Double.parseDouble(itemsRPN.get(i - 1));
+                        break;
+                    }
+                    case "/": {
+                        temp = Double.parseDouble(itemsRPN.get(i - 2)) / Double.parseDouble(itemsRPN.get(i - 1));
+                        break;
+                    }
+                    case "^": {
+                        temp = Math.pow(Double.parseDouble(itemsRPN.get(i - 2)), Double.parseDouble(itemsRPN.get(i - 1)));
+                        break;
+                    }
+                    default: continue;
+                }
+
+                itemsRPN.remove(i - 2);
+                itemsRPN.remove(i - 2);
+                itemsRPN.set(i - 2, "" + temp);
+
+            }
+        }
+
+        return Double.parseDouble(itemsRPN.get(0));
     }
 
     private static int getPriority(String item) {
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,6 +1,8 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorMain {
+    public static final int SPACE = 10;
 
     public static void main(String[] args) {
         Scanner stringScanner = new Scanner(System.in);
@@ -9,6 +11,17 @@ public class CalculatorMain {
 
         Calculator calculator = new Calculator();
 
-        calculator.calculate(expression);
+        double result = calculator.calculate(expression);
+
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.setGroupingUsed(false);
+
+        int beforePoint = ("" + Math.round(result)).length();
+
+        decimalFormat.setMaximumFractionDigits(SPACE - beforePoint - 1);
+
+        String output = decimalFormat.format(result).replace(",", ".");
+
+        System.out.println(output.length() <= SPACE ? output : "Error");
     }
 }
