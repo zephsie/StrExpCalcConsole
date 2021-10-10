0ff1107bfad670749a64cccc4278ac2492d59c2e
zephsie <90218535+zephsie@users.noreply.github.com>
2021-10-10 17:08:34 +0300

    10 10 2021 7

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -20,34 +20,32 @@ public class CalculatorMain {
 
         decimalFormat.setGroupingUsed(false);
 
-        while (true) {
-            System.out.println("Enter your expression:");
+        System.out.println("Enter your expression:");
 
-            String expression = stringScanner.nextLine();
+        String expression = stringScanner.nextLine();
 
-            try {
-                result = calculator.calculate(expression);
+        try {
+            result = calculator.calculate(expression);
 
-                beforePoint = ("" + Math.round(result)).length();
+            beforePoint = ("" + Math.round(result)).length();
 
-                decimalFormat.setMaximumFractionDigits(SPACE - beforePoint - 1);
+            decimalFormat.setMaximumFractionDigits(SPACE - beforePoint - 1);
 
-                output = decimalFormat.format(result).replace(",", ".");
+            output = decimalFormat.format(result).replace(",", ".");
 
-                if (Double.isInfinite(result)) {
-                    System.out.println("You can divide by zero");
-                } else if (Double.isNaN(result)) {
-                    System.out.println("NaN");
-                } else if (output.length() > SPACE) {
-                    System.out.println("Your result is too large");
-                } else {
-                    System.out.println("Your result: " + output);
-                }
-            } catch (CalculatorException e) {
-                System.out.println("Incorrect expression");
+            if (Double.isInfinite(result)) {
+                System.out.println("You can divide by zero");
+            } else if (Double.isNaN(result)) {
+                System.out.println("NaN");
+            } else if (output.length() > SPACE) {
+                System.out.println("Your result is too large");
+            } else {
+                System.out.println("Your result: " + output);
             }
-
-            System.out.println();
+        } catch (CalculatorException e) {
+            System.out.println("Incorrect expression");
         }
+
+        System.out.println();
     }
 }
