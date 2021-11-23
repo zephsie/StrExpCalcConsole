52aee3f1bc170e8791397dee81dd876ab8fd4625
Дима Радионов <radionovdima314@gmail.com>
2021-11-23 23:33:31 +0300

    22 10 2021 убрал лишнюю функцию

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -172,7 +172,7 @@ public class Calculator implements ICalculator {
                 break;
             }
             case "(": {
-                priority = 4;//nesterenkov krutoy
+                priority = 4;
                 break;
             }
             case "^": {
